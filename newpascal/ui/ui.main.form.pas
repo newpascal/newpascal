bd36bda4277c6e5c020319b83c42a285eb5b1dd7
maciej-izak <hnb.code@gmail.com>
2016-10-26 23:50:28 +0200

    Template for NewPascal application

---
+++ newpascal/ui/ui.main.form.pas
@@ -0,0 +1,45 @@
+unit UI.Main.Form;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls, uscaledpi, syncommons;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    ComboBox1: TComboBox;
+    Image1: TImage;
+    lLanguage: TLabel;
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  ScaleDPI(Self, 96);
+  //FindFiles('c:\newpascal\lazarus\languages', '.po
+  //FindFilesDynArrayToFileNames()
+end;
+
+end.
+
